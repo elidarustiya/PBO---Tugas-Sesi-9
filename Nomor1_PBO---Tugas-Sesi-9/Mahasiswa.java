852bc1a69a67e17b5d87a9edeeb6188220699609
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-12-24 13:35:53 +0700

    Add files via upload

---
+++ Nomor1_PBO---Tugas-Sesi-9/Mahasiswa.java
@@ -0,0 +1,34 @@
+/**
+ * Tugas Sesi 9_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Mahasiswa<A,B,C> {
+   private A nim;
+    private B name;
+    private C clas;
+
+    public A getNim(){
+        return nim;
+    }
+
+    public void setNim(A nim){
+        this.nim=nim;
+    }
+
+    public B getName(){
+        return this.name;
+    }
+
+    public void setName(B name){
+        this.name=name;
+    }
+
+    public C getClas(){
+        return clas;
+    }
+
+    public void setClas(C clas){
+        this.clas=clas;
+    }
+}
