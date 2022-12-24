0eb153672bd98fca7353f6922177b59c8c6e3291
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-12-24 13:36:40 +0700

    Add files via upload

---
+++ Nomor2_PBO---Tugas-Sesi-9/GAME/Hero.java
@@ -0,0 +1,12 @@
+/**
+ * Tugas Sesi 9_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public abstract class Hero {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoHero();
+}
---
+++ Nomor2_PBO---Tugas-Sesi-9/MYBOOK/Buku.java
@@ -0,0 +1,35 @@
+/**
+ * Tugas Sesi 9_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Buku {
+    private String judul;
+    private String penulis;
+    private int tahunTerbit;
+
+    Buku(String judul, String penulis, int tahunTerbit){
+        this.judul=judul;
+        this.penulis=penulis;
+        this.tahunTerbit=tahunTerbit;
+    }
+
+    public String getJudul(){
+        return this.judul;
+    }
+
+    public String getNamaPenulis(){
+        return this.penulis;
+    }
+
+    public int getTahunTerbit(){
+        return this.tahunTerbit;
+    }
+
+    public void infoBuku(){
+        System.out.println("Judul\t\t: "+getJudul());
+        System.out.println("Penulis\t\t: "+getNamaPenulis());
+        System.out.println("Tahun Terbit\t: "+getTahunTerbit());
+        System.out.println("");
+    }
+}
